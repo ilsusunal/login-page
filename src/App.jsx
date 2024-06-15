c4bf9fd46a2d65291776d411468cf0c9ab22cc11
ilsu-sunal <sunalilsu@gmail.com>
2024-06-15 14:26:36 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import LoginPage from './page/LoginPage'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <LoginPage/>
+      </main>
+    </>
+  )
+}
+
+export default App
